1aa95b5c67396dfc8411a2a8288a8fe5d7e898e8
Dev <dev@MacBook-Air-de-Deiby.local>
2023-09-05 19:45:51 -0500

    Adding lab2

---
+++ lab2/lab26.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 2.6</title>
+</head>
+
+<body>
+    <?php
+
+    $a = 5;
+    $b = "5";
+    echo ($a == 5) ? "Verdadero" : "falso" , "<br>";
+    echo ($a === 5) ? "Verdadero" : "falso" , "<br>";
+    echo ($b == 5) ? "Verdadero" : "falso" , "<br>";
+    echo ($b === 5) ? "Verdadero" : "falso" , "<br>";
+
+    ?>
+</body>
+
+</html>
